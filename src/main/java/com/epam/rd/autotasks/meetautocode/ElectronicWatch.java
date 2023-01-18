4532d47df20aab29bc5a4e7a5b7d215ba807bc94
Sadique Iqbal <sadiqueiqbal.si@gmail.com>
2023-01-19 00:01:49 +0530

    done

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -6,6 +6,20 @@ public class ElectronicWatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
+        int p1 = seconds % 60;
+        int p2 = seconds / 60;
+        int p3 = p2 % 60;
+
+        p2 = p2 / 60;
+
+        p2= (p2>=24)?0:p2;
+
+        String min = String.format("%02d" , p3);
+        String m=(p3<10)? min : String.valueOf(p3);
+        String sec = String.format("%02d" , p1);
+        String s=(p1<10 && p1>-1)? sec : String.valueOf(p1);
+
+        System.out.print(p2 + ":" + m + ":" + s);
 
 
     }
