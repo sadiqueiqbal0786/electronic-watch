a2dd1d276e004453ec8bca9a7afb16ddd4a0fba0
Sadique Iqbal <sadiqueiqbal.si@gmail.com>
2023-01-19 00:06:35 +0530

    done

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -15,7 +15,7 @@ public class ElectronicWatch {
         p2= (p2>=24)?0:p2;
 
         String min = String.format("%02d" , p3);
-        String m=(p3<10)? min : String.valueOf(p3);
+        String m=(p3<10&&p3<-1)? min : String.valueOf(p3);
         String sec = String.format("%02d" , p1);
         String s=(p1<10 && p1>-1)? sec : String.valueOf(p1);
 
